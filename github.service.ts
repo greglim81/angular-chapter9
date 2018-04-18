531a33032887c0b44f450116ee233ccb807c8f36
Greg Lim <limjunqu@gmail.com>
2018-04-18 09:42:33 +0800

    Update github.service.ts

--- github.service.ts
+++ github.service.ts
@@ -1,15 +1,22 @@
-import {Http} from '@angular/http';
-import 'rxjs/add/operator/map';
-import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
 
-@Injectable()
+export interface GitHubUser{
+    html_url: string;
+    avatar_url: string;
+    login: string;
+    score: string;        
+}
+
+@Injectable() 
 export class GitHubService{
-    constructor(private _http: Http){
+    constructor(private _http: HttpClient){
 
     }
 
-    getGitHubData(_searchTerm){
-        return this._http.get("https://api.github.com/search/users?q=" + _searchTerm)    
-            .map(res => res.json())            
+    getGitHubData(_searchTerm):Observable<GitHubUser>{
+      return this._http.get<GitHubUser>
+        ("https://api.github.com/search/users?q="+_searchTerm);                    
     }
 }
