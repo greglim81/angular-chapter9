f97c91eeecb5687373f0095348924185b54feee6
Greg Lim <limjunqu@gmail.com>
2018-04-18 09:42:06 +0800

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,9 +11,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    ReactiveFormsModule
+    HttpClientModule,
+    ReactiveFormsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
