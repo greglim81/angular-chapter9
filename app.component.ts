9aa45536bb0f8157c8bf2a6dfb77627b3302e4d1
Greg Lim <limjunqu@gmail.com>
2020-04-02 14:22:22 +0800

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -1,48 +1,36 @@
 import { Component } from '@angular/core';
 import { GitHubService } from './github.service'; 
-
 import { filter,debounceTime,distinctUntilChanged } from 'rxjs/operators';
 import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',  
-  styles: [`
-    .img {
-        position: relative;
-        float: left;
-        width:  100px;
-        height: 100px;
-        background-position: 50% 50%;
-        background-repeat:   no-repeat;
-        background-size:     cover;
-    }  
-  `],        
-  template: ` 
-    <input class="form-control" type="search" 
-    [formControl]="searchControl">         
-    <div *ngIf="isLoading">
-      <i class="fa fa-spinner fa-spin fa-3x"></i>
-    </div>   
-    <h3>GitHub User Results</h3>      
-    <div *ngFor="let user of users" class="media">
-        <div class="media-left">
-            <a href="{{ user.html_url }}">
-            <img class="media-object img" 
-              src="{{ user.avatar_url }}" alt="...">
-            </a>
-        </div>
-        <div class="media-body">
-            <h4 class="media-heading">{{ user.login }}</h4>
-            Score: {{ user.score }}               
-        </div>
-    </div>       
+  selector: 'app-root',
+  template: `  
+  <input class="form-control" type="search" [formControl]="searchControl">
+  <h3>GitHub User Results</h3>    
+  <div *ngIf="isLoading">
+    <i class="fa fa-spinner fa-spin fa-3x"></i>
+  </div>    
+  <div *ngFor="let user of users" class="media">
+    <div class="media">
+      <a href="{{ user.html_url }}">
+        <img src="{{ user.avatar_url }}" class="mr-4" alt="..." width="64" height="64">
+      </a>
+      <div class="media-body">
+        <h5 class="mt-0">{{ user.login }}</h5>
+        Score: {{ user.score }}
+      </div>
+    </div>
+  </div>  
   `,
   providers: [GitHubService]
 })
 export class AppComponent {
   searchControl = new FormControl();  
-  isLoading = false; 
-  users = [];
+  isLoading = false;
+
+  users = [];  
+
   constructor(private _githubService: GitHubService){
   }
 
@@ -58,4 +46,4 @@ export class AppComponent {
           });                                  
     });          
   }
-}
+}  
